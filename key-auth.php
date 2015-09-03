de5b292fff1ae2db242d214b26e69abf7a20ec3c
s3rgiosan <me@s3rgiosan.com>
2015-09-03 16:29:29 +0100

    Added entries to regenerate the API secret key and shared secret in the user admin panel

--- key-auth.php
+++ key-auth.php
@@ -35,11 +35,15 @@ class WP_REST_Key_Auth {
 		if ( !isset( $_SERVER['HTTP_X_API_KEY'] ) ||
 			 !isset( $_SERVER['HTTP_X_API_TIMESTAMP'] ) ||
 			 !isset( $_SERVER['HTTP_X_API_SIGNATURE'] ) ) {
+
 			return $user;
 		}
 
-		$user_id     = self::findUserIdByKey( $_SERVER['HTTP_X_API_KEY'] );
-		$user_secret = get_user_meta( $user_id, 'json_shared_secret' );
+		$user_id = self::findUserIdByKey( $_SERVER['HTTP_X_API_KEY'] );
+
+		if ( ! $user_id ) {
+			return false;
+		}
 
 		// Check for the proper HTTP Parameters
 		$signature_args = array(
@@ -49,6 +53,7 @@ class WP_REST_Key_Auth {
 			'request_uri'    => $_SERVER['REQUEST_URI'],
 		);
 
+		$user_secret   = get_user_meta( $user_id, 'json_shared_secret', true );
 		$signature_gen = self::generateSignature( $signature_args, $user_secret );
 		$signature     = $_SERVER['HTTP_X_API_SIGNATURE'];
 
@@ -94,12 +99,102 @@ class WP_REST_Key_Auth {
 		);
 
 		$user = get_users( $user_args );
-		if ( is_array( $user ) && !empty( $user ) ) {
-			return $user[0]->ID;
+
+		if ( empty( $user ) || ! is_array( $user ) ) {
+			return false;
 		}
 
-		return false;
+		return $user[0]->ID;
 	}
+
+	/**
+	 * Add entries to regenerate the API secret key and shared secret in the user admin panel.
+	 *
+	 * @since    0.2.0
+	 * @param    WP_User    $user    User being edited.
+	 */
+	public static function userProfile( $user ) {
+
+		$json_api_key       = get_user_meta( $user->ID, 'json_api_key', true );
+		$json_shared_secret = get_user_meta( $user->ID, 'json_shared_secret', true );
+
+		?>
+		<table class="form-table">
+			<tbody>
+				<tr>
+					<th scope="row"><?php _e( 'REST API Key', 'key-auth' ); ?></th>
+					<td>
+						<?php
+
+							if ( ! empty( $json_api_key ) ) {
+								printf(
+									'<input type="text" name="json_api_key" id="json_api_key" value="%s" disabled="disabled" class="regular-text">',
+									$json_api_key
+								);
+							}
+
+							printf(
+								'<p><label><input type="checkbox" id="reset_json_api_key" name="reset_json_api_key" value="1">%s</label></p>',
+								empty( $json_api_key ) ? __( 'Generate Key', 'key-auth' ) : __( 'Reset Key', 'key-auth' )
+							);
+
+						?>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row"><?php _e( 'REST API Shared Secret', 'key-auth' ); ?></th>
+					<td>
+						<?php
+
+							if ( ! empty( $json_shared_secret ) ) {
+								printf(
+									'<input type="text" name="json_shared_secret" id="json_shared_secret" value="%s" disabled="disabled" class="regular-text">',
+									$json_shared_secret
+								);
+							}
+
+							printf(
+								'<p><label><input type="checkbox" id="reset_json_shared_secret" name="reset_json_shared_secret" value="1">%s</label></p>',
+								empty(  $json_shared_secret ) ? __( 'Generate Shared Secret', 'key-auth' ) : __( 'Reset Shared Secret', 'key-auth' )
+							);
+
+						?>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<?php
+	}
+
+	/**
+	 * Regenerates the user's API key and shared secret.
+	 *
+	 * @since    0.2.0
+	 * @param    int    $user_id    User ID for the user being updated.
+	 */
+	public static function userProfileUpdate( $user_id ) {
+
+		// Regenerate API key
+		$regenerate_api_key = isset( $_POST['reset_json_api_key'] ) && $_POST['reset_json_api_key'];
+		if ( $regenerate_api_key ) {
+			$api_key = wp_generate_password( 32, false );
+			update_user_meta( $user_id, 'json_api_key', $api_key );
+		}
+
+		// Regenerate shared secret
+		$regenerate_shared_secret = isset( $_POST['reset_json_shared_secret'] ) && $_POST['reset_json_shared_secret'];
+
+		if ( $regenerate_shared_secret ) {
+			$shared_secret = wp_generate_password( 32, false );
+			update_user_meta( $user_id, 'json_shared_secret', $shared_secret );
+		}
+	}
+
 }
 
 add_filter( 'determine_current_user',   array( 'WP_REST_Key_Auth', 'authHandler' ), 20 );
+add_action( 'show_user_profile',        array( 'WP_REST_Key_Auth', 'userProfile' ), 90 );
+add_action( 'edit_user_profile',        array( 'WP_REST_Key_Auth', 'userProfile' ), 90 );
+add_action( 'personal_options_update',  array( 'WP_REST_Key_Auth', 'userProfileUpdate' ) );
+add_action( 'edit_user_profile_update', array( 'WP_REST_Key_Auth', 'userProfileUpdate' ) );
