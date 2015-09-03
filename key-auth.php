702bda97443268373dd515b705caa4a160467dcd
s3rgiosan <me@s3rgiosan.com>
2015-09-03 15:16:09 +0100

    Added some PHPDoc and did some code formatting

--- key-auth.php
+++ key-auth.php
@@ -1,17 +1,19 @@
 <?php
 /**
- * Plugin Name: JSON API Key Authentication
- * Description: API/Secret Key Authentication handler for the JSON API
- * Author: Paul Hughes and WP API Team
- * Author URI: https://github.com/WP-API
- * Version: 0.1
- * Plugin URI: https://github.com/WP-API/Key-Auth
+ * Plugin Name: WP REST API Key Authentication
+ * Description: API/Secret Key Authentication handler for the WP REST API
+ * Author: WP REST API Team
+ * Author URI: http://wp-api.org
+ * Version: 0.1.1
+ * Plugin URI: https://github.com/WP-API/WP-API
+ * License: GPL2+
  */
 
 /**
  * Checks the HTTP request and authenticates a user using an API key and shared secret.
  *
- * @param mixed $user The current user passed in the filter.
+ * @since    0.1.0
+ * @param    mixed    $user    The current user passed in the filter.
  */
 
 class JSON_Key_Auth {
@@ -19,35 +21,38 @@ class JSON_Key_Auth {
 	/**
 	 * The primary handler for user authentication.
 	 *
-	 * @param mixed $user The current user (or bool) passing through the filter.
-	 * @return mixed A user on success, or false on failure.
-	 * @author Paul Hughes
+	 * @since     0.1.0
+	 * @param     mixed    $user    The current user (or bool) passing through the filter.
+	 * @return    mixed             A user on success, or false on failure.
 	 */
 	public static function authHandler( $user ) {
+
 		// Don't authenticate twice
 		if ( ! empty( $user ) ) {
 			return $user;
 		}
 
-		if ( !isset( $_SERVER['HTTP_X_API_KEY'] ) || !isset( $_SERVER['HTTP_X_API_TIMESTAMP'] ) || !isset( $_SERVER['HTTP_X_API_SIGNATURE'] ) ) {
+		if ( !isset( $_SERVER['HTTP_X_API_KEY'] ) ||
+			 !isset( $_SERVER['HTTP_X_API_TIMESTAMP'] ) ||
+			 !isset( $_SERVER['HTTP_X_API_SIGNATURE'] ) ) {
 			return $user;
 		}
 
-		$user_id = self::findUserIdByKey( $_SERVER['HTTP_X_API_KEY'] );
+		$user_id     = self::findUserIdByKey( $_SERVER['HTTP_X_API_KEY'] );
 		$user_secret = get_user_meta( $user_id, 'json_shared_secret' );
 
 		// Check for the proper HTTP Parameters
 		$signature_args = array(
-			'api_key' => $_SERVER['HTTP_X_API_KEY'],
-			'timestamp' => $_SERVER['HTTP_X_API_TIMESTAMP'],
+			'api_key'        => $_SERVER['HTTP_X_API_KEY'],
+			'timestamp'      => $_SERVER['HTTP_X_API_TIMESTAMP'],
 			'request_method' => $_SERVER['REQUEST_METHOD'],
-			'request_uri' => $_SERVER['REQUEST_URI'],
+			'request_uri'    => $_SERVER['REQUEST_URI'],
 		);
 
 		$signature_gen = self::generateSignature( $signature_args, $user_secret );
-		$signature = $_SERVER['HTTP_X_API_SIGNATURE'];
+		$signature     = $_SERVER['HTTP_X_API_SIGNATURE'];
 
-		if ( $signature_gen != $signature ) {
+		if ( $signature_gen !== $signature ) {
 			return false;
 		}
 
@@ -55,11 +60,14 @@ class JSON_Key_Auth {
 	}
 
 	/**
-	 * @param array $args The arguments used for generating the signature. They should be, in order:
-	 *                    'api_key', 'timestamp', 'request_method', and 'request_uri'.
-	 *                    Timestamp should be the timestamp passed in the reques.
-	 * @param string $secret The shared secret we are using to generate the hash.
-	 * @return string
+	 * Generate signature.
+	 *
+	 * @since     0.1.0
+	 * @param     array     $args      The arguments used for generating the signature. They should be, in order:
+	 *                                 'api_key', 'timestamp', 'request_method', and 'request_uri'.
+	 *                                 Timestamp should be the timestamp passed in the reques.
+	 * @param     string    $secret    The shared secret we are using to generate the hash.
+	 * @return    string
 	 */
 	public static function generateSignature( $args, $secret ) {
 		return md5( json_encode( $args ) . $secret );
@@ -68,20 +76,23 @@ class JSON_Key_Auth {
 	/**
 	 * Fetches a user ID by API key.
 	 *
-	 * @param string $api_key The API key attached to a user.
-	 * @return bool
+	 * @since     0.1.0
+	 * @param     string    $api_key    The API key attached to a user.
+	 * @return    bool
 	 */
 	public static function findUserIdByKey( $api_key ) {
+
 		$user_args = array(
 			'meta_query' => array(
 				array(
-					'key' => 'json_api_key',
+					'key'   => 'json_api_key',
 					'value' => $api_key,
 				),
 			),
-			'number' => 1,
-			'fields' => array( 'ID' ),
+			'number'     => 1,
+			'fields'     => array( 'ID' ),
 		);
+
 		$user = get_users( $user_args );
 		if ( is_array( $user ) && !empty( $user ) ) {
 			return $user[0]->ID;
